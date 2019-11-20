7e9a8f6060156e8e28e88c401c61b5beab8936f6
po-gl <porterglines@gmail.com>
2019-11-20 10:39:55 -0700

    Implement transformations

--- src/main.rs
+++ src/main.rs
@@ -18,19 +18,42 @@ pub mod file;
 use tuple::Tuple;
 use canvas::Canvas;
 use crate::color::Color;
+use std::f64::consts::PI;
+
 
 fn main() {
-//    let map = vec![vec![0u8; 3]; 2];
-//    println!("Map: {:?}", map);
-//
-//    let mut c = Canvas::new(5, 3);
-//    c.write_pixel(0, 0, &Color::new(1.5, 0.0, 0.0));
-//    c.write_pixel(1, 2, &Color::new(0.0, 0.5, 0.0));
-//    c.write_pixel(2, 4, &Color::new(-0.5, 0.0, 1.0));
-//    let s = c.to_ppm();
-//    println!("String s: {}", s);
-//    file::write_to_file(s, String::from("new.ppm"));
+    draw_clock();
+//    draw_arch();
+}
 
+// Below is miscellaneous functions for testing
+
+fn draw_clock() {
+    let canvas = &mut Canvas::new(100, 100);
+    let color = &Color::new(1.0, 0.0, 0.0);
+    let radius = 3.0/8.0 * canvas.width as f64;
+
+    let center_x = canvas.width / 2;
+    let center_z = canvas.height / 2;
+    let rotate_30_degrees = transformation::rotation_y(PI/6.0);
+
+    let mut mark = tuple::point(0.0, 0.0, 1.0 * radius); // Initial point at 12 o'clock
+
+    for _ in 0..12 {
+        println!("mark: {:?}", mark);
+        // Rotate
+        mark = &rotate_30_degrees * &mark;
+
+        // Draw pixel
+        let x = mark.x.value() as i32 + center_x;
+        let z = mark.z.value() as i32 + center_z;
+        canvas.write_pixel(x, z, color);
+    }
+
+    file::write_to_file(canvas.to_ppm(), String::from("clock.ppm"));
+}
+
+fn draw_arch() {
     let initial_projectile = Projectile {position: tuple::point(0.0, 1.0, 0.0), velocity: tuple::vector(1.0, 1.8, 0.0).normalize() * 11.25};
     let environment = Environment {gravity: tuple::vector(0.0, -0.1, 0.0), wind: tuple::vector(-0.01, 0.0, 0.0)};
 
@@ -39,7 +62,6 @@ fn main() {
     file::write_to_file(canvas.to_ppm(), String::from("projectile.ppm"));
 }
 
-// For Testing Tuples
 struct Projectile {
     position: Tuple,
     velocity: Tuple,
--- src/transformation.rs
+++ src/transformation.rs
@@ -4,6 +4,9 @@
 use super::float::Float;
 use super::matrix::Matrix4;
 
+
+/// Returns a 4x4 matrix used to translate either a tuple or matrix
+/// by multiplication
 pub fn translation(x: f64, y: f64, z: f64) -> Matrix4 {
     let mut new_mat = Matrix4::identity();
     new_mat[0][3] = Float(x);
@@ -12,11 +15,67 @@ pub fn translation(x: f64, y: f64, z: f64) -> Matrix4 {
     new_mat
 }
 
+/// Returns a 4x4 matrix used to scale either a tuple or matrix
+/// by multiplication
+pub fn scaling(x: f64, y: f64, z: f64) -> Matrix4 {
+    let mut new_mat = Matrix4::identity();
+    new_mat[0][0] = Float(x);
+    new_mat[1][1] = Float(y);
+    new_mat[2][2] = Float(z);
+    new_mat
+}
+
+/// Returns a 4x4 matrix used to rotate around the x-axis
+pub fn rotation_x(radians: f64) -> Matrix4 {
+    let mut new_mat = Matrix4::identity();
+    new_mat[1][1] = Float(radians.cos());
+    new_mat[1][2] = Float(-radians.sin());
+    new_mat[2][1] = Float(radians.sin());
+    new_mat[2][2] = Float(radians.cos());
+    new_mat
+}
+
+/// Returns a 4x4 matrix used to rotate around the y-axis
+pub fn rotation_y(radians: f64) -> Matrix4 {
+    let mut new_mat = Matrix4::identity();
+    new_mat[0][0] = Float(radians.cos());
+    new_mat[0][2] = Float(radians.sin());
+    new_mat[2][0] = Float(-radians.sin());
+    new_mat[2][2] = Float(radians.cos());
+    new_mat
+}
+
+/// Returns a 4x4 matrix used to rotate around the z-axis
+pub fn rotation_z(radians: f64) -> Matrix4 {
+    let mut new_mat = Matrix4::identity();
+    new_mat[0][0] = Float(radians.cos());
+    new_mat[0][1] = Float(-radians.sin());
+    new_mat[1][0] = Float(radians.sin());
+    new_mat[1][1] = Float(radians.cos());
+    new_mat
+}
+
+/// Returns a 4x4 matrix used in shearing
+///
+/// x_y denotes "x moved in proportion to y"
+pub fn shearing(x_y: f64, x_z: f64, y_x: f64, y_z: f64, z_x: f64, z_y: f64) -> Matrix4 {
+    let mut new_mat = Matrix4::identity();
+    new_mat[0][1] = Float(x_y);
+    new_mat[0][2] = Float(x_z);
+    new_mat[1][2] = Float(y_z);
+
+    new_mat[1][0] = Float(y_x);
+    new_mat[2][0] = Float(z_x);
+    new_mat[2][1] = Float(z_y);
+    new_mat
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::tuple;
+    use std::f64::consts::PI;
 
     #[test]
     fn transformation_translation() {
@@ -36,4 +95,114 @@ mod tests {
         assert_eq!(t * &v, v); // shouldn't change
     }
 
+    #[test]
+    fn transformation_scale() {
+        // Point
+        let t = scaling(2.0, 3.0, 4.0);
+        let p = tuple::point(-4.0, 6.0, 8.0);
+        assert_eq!(t * p, tuple::point(-8.0, 18.0, 32.0));
+
+        // Vector
+        let t = scaling(2.0, 3.0, 4.0);
+        let p = tuple::vector(-4.0, 6.0, 8.0);
+        assert_eq!(t * p, tuple::vector(-8.0, 18.0, 32.0));
+
+        let t = scaling(2.0, 3.0, 4.0);
+        let inv = t.inverse();
+        let p = tuple::vector(-4.0, 6.0, 8.0);
+        assert_eq!(inv * p, tuple::vector(-2.0, 2.0, 2.0));
+
+        // Reflection
+        let t = scaling(-1.0, 1.0, 1.0);
+        let p = tuple::point(2.0, 3.0, 4.0);
+        assert_eq!(t * p, tuple::point(-2.0, 3.0, 4.0));
+    }
+
+    #[test]
+    fn transformation_rotate() {
+        // x-rotation
+        let p = tuple::point(0.0, 1.0, 0.0);
+        let half_quarter = rotation_x(PI/4.0);
+        let full_quarter = rotation_x(PI/2.0);
+        assert_eq!(half_quarter * &p, tuple::point(0.0, 2.0f64.sqrt()/2.0, 2.0f64.sqrt()/2.0));
+        assert_eq!(full_quarter * &p, tuple::point(0.0, 0.0, 1.0));
+
+        let p = tuple::point(0.0, 1.0, 0.0);
+        let half_quarter = rotation_x(PI/4.0);
+        let inv = half_quarter.inverse();
+        assert_eq!(inv * p, tuple::point(0.0, 2.0f64.sqrt()/2.0, -2.0f64.sqrt()/2.0));
+
+        // y-rotation
+        let p = tuple::point(0.0, 0.0, 1.0);
+        let half_quarter = rotation_y(PI/4.0);
+        let full_quarter = rotation_y(PI/2.0);
+        assert_eq!(half_quarter * &p, tuple::point(2.0f64.sqrt()/2.0, 0.0, 2.0f64.sqrt()/2.0));
+        assert_eq!(full_quarter * &p, tuple::point(1.0, 0.0, 0.0));
+
+        // z-rotation
+        let p = tuple::point(0.0, 1.0, 0.0);
+        let half_quarter = rotation_z(PI/4.0);
+        let full_quarter = rotation_z(PI/2.0);
+        assert_eq!(half_quarter * &p, tuple::point(-2.0f64.sqrt()/2.0, 2.0f64.sqrt()/2.0, 0.0));
+        assert_eq!(full_quarter * &p, tuple::point(-1.0, 0.0, 0.0));
+    }
+
+    #[test]
+    fn transformation_shearing() {
+        // A shearing transformation moves x in proportion to y
+        let s = shearing(1.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+        let p = tuple::point(2.0, 3.0, 4.0);
+        assert_eq!(s * p, tuple::point(5.0, 3.0, 4.0));
+
+        // x in proportion to z
+        let s = shearing(0.0, 1.0, 0.0, 0.0, 0.0, 0.0);
+        let p = tuple::point(2.0, 3.0, 4.0);
+        assert_eq!(s * p, tuple::point(6.0, 3.0, 4.0));
+
+        // y in proportion to x
+        let s = shearing(0.0, 0.0, 1.0, 0.0, 0.0, 0.0);
+        let p = tuple::point(2.0, 3.0, 4.0);
+        assert_eq!(s * p, tuple::point(2.0, 5.0, 4.0));
+
+        // y in proportion to z
+        let s = shearing(0.0, 0.0, 0.0, 1.0, 0.0, 0.0);
+        let p = tuple::point(2.0, 3.0, 4.0);
+        assert_eq!(s * p, tuple::point(2.0, 7.0, 4.0));
+
+        // z in proportion to x
+        let s = shearing(0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+        let p = tuple::point(2.0, 3.0, 4.0);
+        assert_eq!(s * p, tuple::point(2.0, 3.0, 6.0));
+
+        // z in proportion to y
+        let s = shearing(0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
+        let p = tuple::point(2.0, 3.0, 4.0);
+        assert_eq!(s * p, tuple::point(2.0, 3.0, 7.0));
+    }
+
+    #[test]
+    fn transformation_chaining() {
+        let p = tuple::point(1.0, 0.0, 1.0);
+        let a = rotation_x(PI/2.0);
+        let b = scaling(5.0, 5.0, 5.0);
+        let c = translation(10.0, 5.0, 7.0);
+
+        let p2 = a * p;
+        assert_eq!(p2, tuple::point(1.0, -1.0, 0.0));
+
+        let p3 = b * p2;
+        assert_eq!(p3, tuple::point(5.0, -5.0, 0.0));
+
+        let p4 = c * p3;
+        assert_eq!(p4, tuple::point(15.0, 0.0, 7.0));
+
+
+        let p = tuple::point(1.0, 0.0, 1.0);
+        let a = rotation_x(PI/2.0);
+        let b = scaling(5.0, 5.0, 5.0);
+        let c = translation(10.0, 5.0, 7.0);
+        let t = c * b * a;
+        assert_eq!(t * p, tuple::point(15.0, 0.0, 7.0));
+
+    }
 }
