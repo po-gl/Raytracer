d0e96a396613a331603a5bd60fc3afae39a3f5b1
po-gl <porterglines@gmail.com>
2019-12-04 23:47:49 -0700

    Remove dead code comments

--- src/shape/shape_list.rs
+++ src/shape/shape_list.rs
@@ -10,41 +10,6 @@ use std::borrow::BorrowMut;
 /// This is a work-around to implement tree structures with
 /// parents and children referencing each-other
 
-
-//pub trait ShapeT {
-//    fn id(&self) -> i32;
-//    fn debug_fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error>;
-//}
-//impl Debug for Box<dyn ShapeT> {
-//    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-//        self.debug_fmt(f)
-//    }
-//}
-//
-//#[derive(Debug, Clone)]
-//pub struct Ball {
-//    pub id: i32,
-//}
-//
-//impl Ball {
-//    pub fn new(sl: &mut ShapeList) -> Ball {
-//        let id = get_shape_id();
-//        let new_ball = Ball {id};
-//        sl.push(Box::new(new_ball.clone()));
-//        new_ball
-//    }
-//}
-//
-//impl ShapeT for Ball {
-//    fn id(&self) -> i32 {
-//        self.id
-//    }
-//
-//    fn debug_fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-//        write!(f, "Box {:?}", self)
-//    }
-//}
-
 #[derive(Debug)]
 pub struct ShapeList {
     pub shapes: Vec<Box<dyn Shape>>,
@@ -89,24 +54,3 @@ impl IndexMut<usize> for ShapeList {
         &mut self.shapes[index]
     }
 }
-
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//    use crate::shape::plane::Plane;
-//
-//    #[test]
-//    fn shape_list_test() {
-//
-//        let mut sl = ShapeList::new();
-//
-//        let b1 = Ball::new(&mut sl);
-//        let b2 = Ball::new(&mut sl);
-//
-//        println!("{:?}", b1);
-//        println!("{:?}", b2);
-//        println!("{:?}", sl);
-//
-//        assert!(false);
-//    }
-//}
