99331a53190ce00f2a72b6b1be082d08edbf4694
po-gl <porterglines@gmail.com>
2019-12-05 06:45:32 -0700

    Add normal perturb properties to materials

--- src/material.rs
+++ src/material.rs
@@ -16,6 +16,8 @@ pub struct Material {
     pub transparency: Float,
     pub refractive_index: Float,
     pub pattern: Option<Box<dyn Pattern>>,
+    pub normal_perturb: Option<String>,
+    pub normal_perturb_factor: Option<f64>,
 }
 
 impl Material {
@@ -28,7 +30,8 @@ impl Material {
                   reflective: Float(0.0),
                   transparency: Float(0.0),
                   refractive_index: Float(1.0),
-                  pattern: None}
+                  pattern: None, normal_perturb: None,
+                  normal_perturb_factor: None}
     }
 
     pub fn set_pattern(&mut self, pattern: Box<dyn Pattern>) {
@@ -46,10 +49,11 @@ impl Material {
             reflective: Float(0.8),
             transparency: Float(1.0),
             refractive_index: Float(1.5),
-            pattern: None}
-    }
+            pattern: None, normal_perturb: None,
+            normal_perturb_factor: None}
+}
 
-    pub fn mirror() -> Material {
+pub fn mirror() -> Material {
         Material {color: Color::new(0.9, 0.9, 1.0),
             ambient: Float(0.1),
             diffuse: Float(0.1),
@@ -58,11 +62,12 @@ impl Material {
             reflective: Float(1.0),
             transparency: Float(0.0),
             refractive_index: Float(1.0),
-            pattern: None}
-    }
+            pattern: None, normal_perturb: None,
+            normal_perturb_factor: None}
+}
 
 
-    // Common material values
+// Common material values
 
     pub fn water_refractive_index() -> Float {
         Float(1.333)
@@ -82,7 +87,7 @@ impl Material {
 mod tests {
     use super::*;
     use crate::tuple::{vector, point};
-    use crate::light::{Light, lighting};
+    use crate::light::{Light};
     use crate::shape::sphere::Sphere;
     use crate::pattern::stripe_pattern::StripePattern;
     use crate::shape::shape_list::ShapeList;
@@ -110,8 +115,8 @@ mod tests {
         let eyev = vector(0.0, 0.0, -1.0);
         let normalv = vector(0.0, 0.0, -1.0);
         let light = Light::point_light(&point(0.0, 0.0, -10.0), &Color::white());
-        let c1 = lighting(&m, Some(Box::new(object.clone())), &light, &point(0.9, 0.0, 0.0), &eyev, &normalv, false);
-        let c2 = lighting(&m, Some(Box::new(object.clone())), &light, &point(1.1, 0.0, 0.0), &eyev, &normalv, false);
+        let c1 = Light::lighting(&m, Some(Box::new(object.clone())), None, &light, &point(0.9, 0.0, 0.0), None, &eyev, &normalv, false, None);
+        let c2 = Light::lighting(&m, Some(Box::new(object.clone())), None, &light, &point(1.1, 0.0, 0.0), None, &eyev, &normalv, false, None);
         assert_eq!(c1, Color::white());
         assert_eq!(c2, Color::black());
     }
